729f260935d4f016b53b9189e57f41764ee5c925
DuaFilla <pka1109trans@gmail.com>
2022-04-24 21:16:34 +0300

    Добавил несколько функций

--- sandbox/src/main/java/lrn/java1/sandbox/Hi.java
+++ sandbox/src/main/java/lrn/java1/sandbox/Hi.java
@@ -2,8 +2,29 @@ package lrn.java1.sandbox;
 
 public class Hi {
 
+  public static void hello(String somebody) {
+    System.out.println("Hello," + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
   public static void main(String[] args) {
-    System.out.println(" Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Kirill");
+
+    double l = 5;
+    System.out.println("Ploshad kvadrata so storonoi " + l + " = " + area(l));
+
+    double a = 5;
+    double b = 6;
+    System.out.println("Ploshad pryamougolnika so storonami " + a + " i " + b + " = " + area(a, b));
   }
 
 }
