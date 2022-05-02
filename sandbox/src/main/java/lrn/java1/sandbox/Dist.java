9a244f1758529734b92e32bbc61b3d37e6c5eb91
DuaFilla <pka1109trans@gmail.com>
2022-05-02 13:27:44 +0300

    Тренируюсь

--- sandbox/src/main/java/lrn/java1/sandbox/Dist.java
+++ sandbox/src/main/java/lrn/java1/sandbox/Dist.java
@@ -1,7 +1,20 @@
 package lrn.java1.sandbox;
 
 public class Dist {
-  public static void main(String[] args) {
+
 
   }
+
+    public static void main(String[] args) {
+      Point p1 = new Point();
+      p1.x = 2;
+      p1.y = 3;
+      Point p2 = new Point();
+      p2.x = 4;
+      p2.y = 5;
+
+      System.out.println("Расстояние между точками А(" + p1.x + ";" + p1.y + ") и В( " + p2.y + ";" +
+        p2.y + ") = " + distance(p1, p2));
+
+    }
 }
--- sandbox/src/main/java/lrn/java1/sandbox/Point.java
+++ sandbox/src/main/java/lrn/java1/sandbox/Point.java
@@ -1,25 +1,14 @@
 package lrn.java1.sandbox;
 
 public class Point {
-  public double x1;
-  public double x2;
-  public double y1;
-  public double y2;
+  public double x;
+  public double y;
 
+  public Point(double p) {
+    this.p1 = p
+  }
   public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p2.x2 - p1.x1) * (p2.x2 - p1.x1)) + ((p2.y2 - p1.y1) * (p2.y2 - p1.y1)));
-  }
+    return Math.sqrt(((p2.y - p1.x) * (p2.y - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
 
-  public static void main(String[] args) {
-    Point p1 = new Point();
-    p1.x1 = 2;
-    p1.y1 = 3;
-    Point p2 = new Point();
-    p2.x2 = 4;
-    p2.y2 = 5;
 
-    System.out.println("Расстояние между точками А(" + p1.x1 + ";" + p1.y1 + ") и В( " + p2.x2 + ";" +
-      p2.y2 + ") = " + distance(p1, p2));
-
-  }
 }
