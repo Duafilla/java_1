b1ec6cde957a64f2f1df7b8a48d1c90edbe2e131
DuaFilla <pka1109trans@gmail.com>
2022-05-02 21:09:35 +0300

    Создал первый авто-тест

---
+++ sandbox/src/test/java/lrn/java/sandbox/SquareTests.java
@@ -0,0 +1,14 @@
+package lrn.java.sandbox;
+
+import lrn.java1.sandbox.Square;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+
+  @Test
+  public void testArea() {
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(),25);
+  }
+}
