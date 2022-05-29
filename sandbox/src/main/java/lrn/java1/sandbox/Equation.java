3934977d4bce9b2da6494a9d4d60655abf8f9ca8
DuaFilla <pka1109trans@gmail.com>
2022-05-29 09:31:06 +0300

    Промежуток

--- sandbox/src/main/java/lrn/java1/sandbox/Equation.java
+++ sandbox/src/main/java/lrn/java1/sandbox/Equation.java
@@ -15,30 +15,30 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
-      } else {
+      } else{
         if (d == 0) {
           n = 1;
         } else {
           n = 0;
         }
       }
+    } else {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else{
+        n = 1;
+      }
     }
   }
 
-    public int rootNumber () {
-      return n;
-    }
+  public int rootNumber() {
+    return n;
   }
+}
