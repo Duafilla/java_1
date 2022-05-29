c92400f793e34304ef4b489dda24892018ed25d5
DuaFilla <pka1109trans@gmail.com>
2022-05-29 12:38:55 +0300

    В процессе добавления мозилы и эджа

--- addressbook-web-tests/src/test/java/lrn/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/appmanager/ApplicationManager.java
@@ -2,6 +2,9 @@ package lrn.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,10 +15,25 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
+    if(browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else {
+      if (browser == BrowserType.FIREFOX) {
+        wd = new FirefoxDriver();
+      } else {
+        wd = new EdgeDriver();
+      }
+    }
     System.setProperty("webdriver.chrome.driver", "c:\\windows\\system32\\chromedriver.exe");
-    wd = new ChromeDriver();
+    System.setProperty("webdriver.firefox.driver", "c:\\windows\\system32\\geckodriver.exe");
+    System.setProperty("webdriver.MicrosoftEdge.driver", "c:\\windows\\system32\\msedgedriver.exe");
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
--- addressbook-web-tests/src/test/java/lrn/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package lrn.addressbook.tests;
 
 import lrn.addressbook.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
