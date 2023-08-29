940e89facf771e672311cef33bf60ddf8a5c30fa
ProkhorovKA <ProkhorovKA@gnivc.ru>
2023-08-29 16:49:52 +0300

    Начинаю делать проверку на создание

--- addressbook-web-tests/src/test/java/lrn/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package lrn.addressbook.tests;
 
 import lrn.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
@@ -8,8 +9,11 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
 
+    int before = app.getGroupHelper().getGroupCount();
     app.getNavigationHelper().goToGroupPage();
     app.getGroupHelper().createGroup(new GroupData("111", "111", "111"));
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 }
