2418bcbef428cc446b8d187519faee768a8e6a5f
ProkhorovKA <ProkhorovKA@gnivc.ru>
2023-09-27 18:00:24 +0300

    Чуть доработал,удалил лишний код (компоратор и сортировка)

--- addressbook-web-tests/src/test/java/lrn/addressbook/tests/ContactDeleteTests.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/tests/ContactDeleteTests.java
@@ -24,9 +24,6 @@ public class ContactDeleteTests extends TestBase {
     Assert.assertEquals(before.size() - 1,after.size());
 
     before.remove(0);
-    Comparator<ContactData> comparator = Comparator.comparing(ContactData::getId);
-    before.sort(comparator);
-    after.sort(comparator);
     Assert.assertEquals(before,after);
 
 
