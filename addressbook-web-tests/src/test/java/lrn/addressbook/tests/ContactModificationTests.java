ec593045bade5900318ed43b74e2c0918d8cb8d3
ProkhorovKA <ProkhorovKA@gnivc.ru>
2023-09-27 10:56:10 +0300

    При модификации контакт падает в самый низ таблицы,это ломает логику,видимо надо сортировать два раза

--- addressbook-web-tests/src/test/java/lrn/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/tests/ContactModificationTests.java
@@ -1,7 +1,11 @@
 package lrn.addressbook.tests;
 
 import lrn.addressbook.model.ContactData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class ContactModificationTests extends TestBase{
 
@@ -13,11 +17,21 @@ public class ContactModificationTests extends TestBase{
       app.getContactHelper().createContact(new ContactData( "qwerty", "qwerty",
               "qwerty", "11111", "111111", "111111", "222222ddddd", "qwerty"), true);
     }
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData( "www", "www", "2222", "121ddd", "fff",
-            "222", "qw", null),false);
+    ContactData contactData = new ContactData( "www", "www", "2222", "121ddd", "fff", "222", "qw", null);
+    app.getContactHelper().fillContactForm(contactData,false);
     app.getContactHelper().updateContact();
     app.getNavigationHelper().goToHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(before.size(),after.size());
+
+    before.remove(before.size() - 1);
+    before.add(contactData);
+    Comparator<ContactData> comparator = Comparator.comparing(ContactData::getId);
+    before.sort(comparator);
+    after.sort(comparator);
+    Assert.assertEquals(before,after);
   }
 
 
