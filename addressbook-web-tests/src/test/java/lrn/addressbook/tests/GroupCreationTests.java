3fc672243e48a43676636de8c6cd72c98232f41e
ProkhorovKA <ProkhorovKA@gnivc.ru>
2023-10-29 11:50:24 +0300

    Добавил метод,который передаёт тестовые данные в тестовый метод(пока только введённые строки)

--- addressbook-web-tests/src/test/java/lrn/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/tests/GroupCreationTests.java
@@ -2,17 +2,29 @@ package lrn.addressbook.tests;
 
 import lrn.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<>();
+    list.add(new Object[] {new GroupData("test1","header1", "footer1")});
+    list.add(new Object[] {new GroupData("test2","header2", "footer2")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData groupData) {
 
     app.goTo().groupPage();
     Set<GroupData> before = app.group().all();
-    GroupData groupData = new GroupData("112", "111", "111");
     app.group().create(groupData);
     Assert.assertEquals(app.group().getGroupCount(), before.size() + 1);
 
@@ -24,7 +36,7 @@ public class GroupCreationTests extends TestBase {
   }
 
   @Test
-  public void testBadGroupCreation() throws Exception {
+  public void testBadGroupCreation() {
 
     app.goTo().groupPage();
     Set<GroupData> before = app.group().all();
