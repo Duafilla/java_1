4bf121d5c8694208ef58f5ecb95999711fc7996c
ProkhorovKA <ProkhorovKA@gnivc.ru>
2024-01-02 13:09:15 +0300

    Добавил свойства (перенёс в отдельные переменные вещи типа логина и пароля, а также ссылку)

--- addressbook-web-tests/src/test/java/lrn/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/appmanager/ApplicationManager.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+  private final Properties properties;
   protected WebDriver wd;
   private ContactHelper contactHelper;
   private NavigationHelper navigationHelper;
@@ -18,10 +24,14 @@ public class ApplicationManager {
 
   public ApplicationManager(String browser) {
     this.browser = browser;
+    properties = new Properties();
+
   }
 
-  public void init() {
+  public void init() throws IOException {
 
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
     System.setProperty("webdriver.chrome.driver", "c:\\windows\\system32\\chromedriver.exe");
     System.setProperty("webdriver.firefox.driver", "c:\\windows\\system32\\geckodriver.exe"); /*вообще можно эту строку удалить т.к. в else есть ссылка на драйвер*/
 
@@ -36,12 +46,12 @@ public class ApplicationManager {
 
 
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     contactHelper = new ContactHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
 
   }
 
