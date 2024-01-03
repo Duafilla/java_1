cc73c80a673f24a3221864004aa0572eedbb1b50
ProkhorovKA <ProkhorovKA@gnivc.ru>
2024-01-03 16:55:47 +0300

    Добавил запросы через гибернейт,н опока не работает

--- addressbook-web-tests/src/test/java/lrn/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/lrn/addressbook/model/GroupData.java
@@ -3,18 +3,35 @@ package lrn.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
 @XStreamAlias("group")
+@Entity
+@Table(name = "group_list")
 public class GroupData {
     @XStreamOmitField
+    @Id
+    @Column(name = "group_id")
     private int id;
+
     @Expose
+    @Column(name = "group_name")
     private final String name;
+
     @Expose
+    @Column(name = "group_header")
+    @Type(type = "text")
     private final String header;
+
     @Expose
+    @Column(name = "group_footer")
+    @Type(type = "text")
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
